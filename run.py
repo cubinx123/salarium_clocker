d24c27c8c0ed9e992743eb118857b01037bc02b8
Lester Paja <lester.paja@entrego.com.ph>
2021-01-18 18:51:46 +0800

    fixed clockout

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import time
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -11,29 +15,25 @@ salarium_creds = {'email': os.environ.get("email"),
                   'password': os.environ.get("password")
                   }
 
-driver_details = {
-                  'driver': os.environ.get("driver_name"),
-                  'driver_path': os.path.dirname(os.path.abspath(__file__)) + '\\'
-                  }
-
-# print(driver_details['driver_path'] + driver_details['driver'])
-
 
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get('https://app.salarium.com/users/login')
-time.sleep(3)
+time.sleep(5)
 
 email_textbox = driver.find_element_by_name("email")
 email_textbox.send_keys(salarium_creds['email'])
-time.sleep(5)
+time.sleep(2)
 
 password_textbox = driver.find_element_by_name("password")
 password_textbox.send_keys(salarium_creds['password'])
-time.sleep(5)
+time.sleep(2)
 
 login_button = driver.find_element_by_class_name("btn-form-custom")
 login_button.submit()
-time.sleep(15)
+time.sleep(10)
 
-clock_button = driver.find_element_by_id("time_btn")
+clock_button = driver.find_element_by_xpath("//div[@class='button-group']//button[@id='time_btn']")
 clock_button.click()
+time.sleep(2)
+driver.quit()
+
